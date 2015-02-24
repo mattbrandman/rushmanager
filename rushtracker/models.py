9c874580f03a04b5a85aca8650a9580b855f03c0
farid00 <brandman.m@husky.neu.edu>
2015-02-24 16:55:41 -0500

    only rush name is required

--- rushtracker/models.py
+++ rushtracker/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 class Rush(models.Model):
-	name = models.CharField('Name', max_length=200)
+	name = models.CharField('Name', max_length=200, )
 	contact = models.ForeignKey(User, verbose_name='Brotherhood Contact', limit_choices_to={'is_staff': False}, blank=True, null=True)
-	number = models.CharField('Phone Number', max_length=30)
-	facebook_link = models.CharField('FaceBook Link', max_length = 200)
-	contacted_date = models.DateField('Date Contacted')
-	email_address = models.EmailField('Email Address', max_length=100)
-	rank = models.IntegerField('Rank', default = 5)
+	number = models.CharField('Phone Number', max_length=30, blank=True, null=True)
+	facebook_link = models.CharField('FaceBook Link', max_length = 200, blank=True, null=True)
+	contacted_date = models.DateField('Date Contacted', blank=True, null=True)
+	email_address = models.EmailField('Email Address', max_length=100, blank=True, null=True)
+	rank = models.IntegerField('Rank', default = 5, blank=True, null=True)
 	
 	def __str__(self):
 		return self.name
