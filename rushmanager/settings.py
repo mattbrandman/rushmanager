e017bece1891a624b88dad2ed6dac1848779f8a6
farid00 <brandman.m@husky.neu.edu>
2015-03-02 19:43:59 -0500

    External Storage is up and running!

--- rushmanager/settings.py
+++ rushmanager/settings.py
@@ -7,7 +7,6 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -50,6 +49,7 @@ INSTALLED_APPS = (
     'comments',
     'events',
     'braces',
+    'storages',
     'authentication',
     'organization',
 )
@@ -121,3 +121,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AWS_STORAGE_BUCKET_NAME = 'rushmanagerbucket'
+AWS_ACCESS_KEY_ID = 'AKIAIA3LGLE6OGN3ELOQ'
+AWS_SECRET_ACCESS_KEY = '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'
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'rushmanager.custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'rushmanager.custom_storages.MediaStorage'
