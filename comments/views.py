745f649e502d508f6582f6c8e747a15008f2b887
farid00 <brandman.m@husky.neu.edu>
2015-03-10 00:38:18 -0400

    forgot a comma and screwed up an indent

--- comments/views.py
+++ comments/views.py
@@ -10,10 +10,10 @@ from authentication.mixins import CorrectOrganizationMixin
 import pdb
 
 
-class CommentListView(LoginRequiredMixin, CorrectOrganizationMixin  generic.ListView):
+class CommentListView(LoginRequiredMixin, CorrectOrganizationMixin, generic.ListView):
     template_name = 'comments/comment_list.html'
     context_object_name = 'rush_comments'
-	def dispatch(self, request, *args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
 		self.organization = get_object_or_404(Rush, kwargs['pk']).organization
 		return super(CommentListView, self).dispatch(request, *args, **kwargs)
 
