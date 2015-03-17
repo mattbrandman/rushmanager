0fe215e769b1b6a1e30aae3c9607e147506238f9
farid00 <brandman.m@husky.neu.edu>
2015-03-16 21:27:50 -0400

    Heroku again

--- rushmanager/settings.py
+++ rushmanager/settings.py
@@ -142,8 +142,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_LOCATION = 'static'
 #STATICFILES_STORAGE = 'rushmanager.custom_storages.StaticStorage'
 #STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-
-
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'rushmanager.custom_storages.MediaStorage'
