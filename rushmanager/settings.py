0144be7494510d0a15389540215605d48d542031
farid00 <brandman.m@husky.neu.edu>
2015-03-16 20:56:36 -0400

    asdf

--- rushmanager/settings.py
+++ rushmanager/settings.py
@@ -93,8 +93,13 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {'default', dj_database_url.config()}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
