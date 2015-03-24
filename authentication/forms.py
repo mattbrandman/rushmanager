9dd2fecf7619f29ec4cba3521ce78fe8990740fa
farid00 <brandman.m@husky.neu.edu>
2015-03-24 02:56:35 -0400

    custom user models up and running, nothing special about them yet just no more username.

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,52 +4,81 @@ from organization.models import Organization
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from authentication.models import UserProfile
-from django.contrib.auth.models import User, Permission
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
+from django.contrib.auth.models import Permission
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from crispy_forms.layout import Layout, Field, ButtonHolder, Submit, Hidden
 from django import forms
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
 from django.forms.util import ErrorList
 from organization.forms import CreateOrganizationForm
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.forms.models import ModelForm
 import pdb
 
+
 class UserSignInForm(AuthenticationForm):
+
     def __init__(self, *args, **kwargs):
         super(UserSignInForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.render_hidden_fields = True
         self.helper.form_tag = False
-        self.helper.add_input(Submit('submit', 'Submit', css_class='btn_primary'))
+        self.helper.add_input(
+            Submit('submit', 'Submit', css_class='btn_primary'))
 
-class ChapterAdminForm(UserCreationForm):
-    first_name = forms.CharField(max_length=50, required=False)
-    last_name = forms.CharField(max_length=50, required=False)
+
+class ChapterAdminForm(ModelForm):
+    name = forms.CharField(max_length=50, required=False)
     fraternity = forms.CharField(max_length=100, required=True)
     chapter = forms.CharField(max_length=100, required=True)
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'username', 
-            'email', 'password1', 'password2')
-    helper = FormHelper()
-    helper.add_input(Submit('Submit', 'Submit', css_class='btn-primary'))
-    def save(self, commit=True):
-        if not commit:
-            raise NotImplementedError("Can't create User and UserProfile without database save")
-        user = super(ChapterAdminForm, self).save(commit=True)
-        user.user_permissions.add(Permission.objects.get(codename='chapter_admin'),)
-        data = {
-            'owner' : user.id,
-            'national_organization' : self.cleaned_data['fraternity'], 
-            'chapter_name' : self.cleaned_data['chapter']
-        }
-        organization = CreateOrganizationForm(data)
-        my_org = organization.save()
-        user_profile = UserProfile(user=user, organization=my_org)
-        user_profile.save()
-        return user
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput)
 
-class UserProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ChapterAdminForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.add_input(
+            Submit('Submit', 'Submit', css_class='btn-primary'))
+
+    def clean_password2(self):
+            # Check that the two password entries match
+            password1 = self.cleaned_data.get("password1")
+            password2 = self.cleaned_data.get("password2")
+            if password1 and password2 and password1 != password2:
+                msg = "Passwords don't match"
+                raise forms.ValidationError("Password mismatch")
+            return password2
+    def save(self, commit=True):
+            if not commit:
+                raise NotImplementedError("Can't create User and UserProfile without database save")
+            user = super(ChapterAdminForm, self).save(commit=False)
+            if self.cleaned_data['name']:
+                first_last = self.cleaned_data['name'].split(" ")
+                user.first_name = first_last[0]
+                user.last_name = first_last [1]
+            user.set_password(self.cleaned_data["password1"])
+            user.save()
+            user.user_permissions.add(Permission.objects.get(codename='chapter_admin'),)
+            data = {
+                'owner' : user.id,
+                'national_organization' : self.cleaned_data['fraternity'], 
+                'chapter_name' : self.cleaned_data['chapter']
+            }
+            organization = CreateOrganizationForm(data)
+            my_org = organization.save()
+            user_profile = UserProfile(user=user, organization=my_org)
+            user_profile.save()
+            return user
+
+    class Meta:
+        model = get_user_model()
+        fields = ('name', 'email', 'password1', 'password2')
+
+
+class UserProfileForm(ModelForm):
     class Meta:
         model = UserProfile
         exclude = ('',)
@@ -60,8 +89,8 @@ class SingleUserCreationForm(UserCreationForm):
     helper = FormHelper()
     helper.form_action = '/authentication/createSingleUser'
     class Meta:
-        model = settings.AUTH_USER_MODEL
-        fields = ('first_name', 'last_name', 'username', 
+        model = get_user_model()
+        fields = ('first_name', 'last_name',
             'email', 'password1', 'password2')
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
--- authentication/models.py
+++ authentication/models.py
@@ -1,25 +1,55 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, PermissionsMixin
 from organization.models import Organization
-from django.contrib.auth.models import Permission, AbstractBaseUser 
+from django.contrib.auth.models import Permission, AbstractBaseUser, BaseUserManager
 from django.utils import timezone
 from django.db import connection
 from django.conf import settings
-class BrotherUser(AbstractBaseUser):
+
+class BrotherUserManager(BaseUserManager):
+
+	def	_create_user(self, email, password, 
+		is_staff, is_superuser, **extra_fields):
+		now = timezone.now()
+		if not email:
+			raise ValueError('The given email must be set')
+		email = self.normalize_email(email)
+		user = self.model(email=email,
+			is_staff=is_staff, is_active=True,
+			is_superuser=is_superuser, last_login=now,
+			date_created=now, **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+
+	def create_user(self, email, password=None, **extra_fields):
+		return self._create_user(email, password, False, False, **extra_fields)
+
+	def create_superuser(self, email, password, **extra_fields):
+		return self._create_user(email, password, True, True, **extra_fields)
+
+class BrotherUser(AbstractBaseUser, PermissionsMixin):
 	email = models.CharField(max_length=100, unique=True)
 	is_staff = models.BooleanField(default=False)
 	first_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=30)
 	date_created = models.DateTimeField(default=timezone.now, blank=True)
+	is_active = models.BooleanField(default=True)
 	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = []
+	objects = BrotherUserManager()
+	def get_full_name():
+		return first_name + " " + last_name
+
+	def get_short_name():
+		return first_name
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="profile")
-	organization = models.ForeignKey(Organization)
+	organization = models.ForeignKey('organization.Organization')
 	class Meta:
 		permissions = (
 			("chapter_admin", "Can access chapter admin page"),
 		)
 	def __str__(self):
 		return self.user.username
-
