d8401075f6dca5c4f8b0ff61ee186b419510d314
farid00 <brandman.m@husky.neu.edu>
2015-04-15 17:28:48 -0400

    More ranking tools initial commit ;

--- rushtracker/static/app.js
+++ rushtracker/static/app.js
@@ -1,11 +1,44 @@
 (function(){
 	var app = angular.module('rankingCreation', []);
-	app.controller('RankingController', ['$scope', '$http', function( $scope, $http){
-		$scope.listOfRushes = [{"first_name":"kmkll","last_name":"","id":1},];
-			$http.get('/api/rush/').
+	app.controller('RankingController', ['$scope', '$http', '$q', 'httpRushService', function( $scope, $http, $q, httpRushService){
+		this.ranking = {};
+		this.rankList = this.range;
+		//this is the list of rankings
+		this.rankingList = [];
+		var _this = this
+		var x = httpRushService.getRushes();
+		x.then(function(payload) {
+			_this.listOfRushes = payload.data;
+			_this.rankList = _this.range();
+		});
+
+		this.range = function() {
+			var result = [];
+			var i;
+			for(i = 1; i <= this.listOfRushes.length; i++) {
+				result.push(i);
+			}
+			return result;
+		};
+
+		this.addRanking = function() {
+			this.rankingList.push(this.ranking);
+			this.listOfRushes.splice(this.listOfRushes.indexOf(this.ranking.rush), 1);
+			this.rankList.splice(this.rankList.indexOf(this.ranking.rank), 1);
+			this.ranking = {};
+			
+		};
+
+		
+	}]);
+
+
+	app.service('httpRushService', ['$http', function($http) {
+		this.getRushes = function() {
+			return $http.get('/api/rush/').
 			success(function(data, status, headers, config){
 			//something funk with scopes and how data is formatted when returned
-				$scope.listOfRushes = data;
-			});
-		}]);
+		});
+		}
+	}]);
 })();
