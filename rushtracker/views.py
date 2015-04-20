d163a0ac61ba7fb7cb30e0908fd09ce06b6bd9e5
farid00 <brandman.m@husky.neu.edu>
2015-04-20 14:56:47 -0400

    stupid matt fix

--- rushtracker/views.py
+++ rushtracker/views.py
@@ -27,7 +27,7 @@ class UpdateView(LoginRequiredMixin, CorrectOrganizationMixin, generic.UpdateVie
 	def get_success_url(self):
 		return reverse('rushtracker:index')
 	def get_form_kwargs(self):
-		kwargs = super(RushCreateView, self).get_form_kwargs()
+		kwargs = super(UpdateView, self).get_form_kwargs()
 		kwargs['request'] = self.request
 		return kwargs
 	def dispatch(self, request, *args, **kwargs):
