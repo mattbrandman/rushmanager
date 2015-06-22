d0da5a4872a424c7d449db2e27927669c97b17c2
farid00 <brandman.m@husky.neu.edu>
2015-06-22 15:47:22 -0400

    adding ability to inline a single initial comment

--- rushtracker/forms.py
+++ rushtracker/forms.py
@@ -11,6 +11,7 @@ from django.core.urlresolvers import reverse
 from base64 import b64decode
 from django.core.files.base import ContentFile
 from django.contrib.auth import get_user_model
+from comments.models import Comment
 import uuid
 
 
@@ -31,13 +32,15 @@ class UpdateForm(ModelForm):
             'primary_contact'].queryset = get_user_model().tenant_objects.all().order_by('email')
         self.fields[
             'secondary_contact'].queryset = get_user_model().tenant_objects.all().order_by('email')
-        self.fields['rush_period'].queryset = self.request.user.organization.rushperiod_set.all()
+        self.fields[
+            'rush_period'].queryset = self.request.user.organization.rushperiod_set.all()
         self.helper = FormHelper(self)
         self.helper['rush_period'].wrap(Field, css_class="chosen-select")
         self.helper.add_input(
             Submit('submit', 'Update', css_class='btn-primary'))
         self.helper['primary_contact'].wrap(Field, css_class="update-select2")
-        self.helper['secondary_contact'].wrap(Field, css_class="update-select2")
+        self.helper['secondary_contact'].wrap(
+            Field, css_class="update-select2")
 
     def save(self, commit=True):
         if not commit:
@@ -50,11 +53,12 @@ class UpdateForm(ModelForm):
             rush.picture = ContentFile(image_data, image_name)
         self.save_m2m()
         rush.save()
-        print rush.picture
 
 
 class CreateRushForm(forms.ModelForm):
     pic64Value = forms.CharField(required=False, widget=forms.HiddenInput())
+    comment = forms.CharField(
+        label='Initial Comment', widget=forms.Textarea, required=False)
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
@@ -63,16 +67,19 @@ class CreateRushForm(forms.ModelForm):
             'primary_contact'].queryset = get_user_model().tenant_objects.all()
         self.fields[
             'secondary_contact'].queryset = get_user_model().tenant_objects.all()
-        self.fields['rush_period'].queryset = self.request.user.organization.rushperiod_set.all()
+        self.fields[
+            'rush_period'].queryset = self.request.user.organization.rushperiod_set.all()
         if self.request.user.organization.active_rush_period:
-            self.fields['rush_period'].initial = ([self.request.user.organization.active_rush_period, ])
+            self.fields['rush_period'].initial = (
+                [self.request.user.organization.active_rush_period, ])
         self.helper = FormHelper(self)
         self.helper['contacted_date'].wrap(Field, css_class="datepicker")
         self.helper['rush_period'].wrap(Field, css_class="chosen-select")
         self.helper.add_input(
             Submit('submit', 'Submit', css_class='btn-primary'))
         self.helper['primary_contact'].wrap(Field, css_class="create-select2")
-        self.helper['secondary_contact'].wrap(Field, css_class="create-select2")
+        self.helper['secondary_contact'].wrap(
+            Field, css_class="create-select2")
 
     def save(self, commit=True):
         if not commit:
@@ -84,11 +91,19 @@ class CreateRushForm(forms.ModelForm):
             image_data = b64decode(self.request.POST['pic64Value'][22:])
             image_name = str(uuid.uuid1())
             rush.picture = ContentFile(image_data, image_name)
-        
-        else: 
+
+        else:
             rush.picture = 'profile_picture/default_picture.png'
         rush.save()
         self.save_m2m()
+        if self.cleaned_data['comment'] != None:
+            newComment = Comment()
+            newComment.comment = self.cleaned_data['comment']
+            newComment.organization = self.request.user.organization
+            newComment.rush_period = self.request.user.organization.active_rush_period
+            newComment.user = self.request.user
+            newComment.rush = rush
+            newComment.save()
 
     class Meta:
         model = Rush
