f67a20391712d04d1ce57a5cdfc510c5c7931c39
farid00 <brandman.m@husky.neu.edu>
2015-06-24 23:15:16 -0400

    user models

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from authentication.models import UserProfile
+from authentication.models import BrotherUser
 
 admin.site.register(UserProfile)
+admin.site.register(BrotherUser)
