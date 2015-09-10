899de04984a39b2c4d6f65c3f82b5b304b3393da
farid00 <brandman.m@husky.neu.edu>
2015-09-10 17:24:01 -0400

    thing

--- rushmanager/urls.py
+++ rushmanager/urls.py
@@ -16,7 +16,6 @@ from rest_framework.decorators import detail_route
 from authentication.permissions import SameOrganizationPermission
 from api import serializers
 from landing.views import HomePage
-# Serializers define the API representation.
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
