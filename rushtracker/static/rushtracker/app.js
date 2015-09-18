444ee7fe74338eee2855aaba4bdefba3ab5150e4
farid00 <brandman.m@husky.neu.edu>
2015-09-17 22:46:26 -0400

    updating index

--- rushtracker/static/rushtracker/app.js
+++ rushtracker/static/rushtracker/app.js
@@ -2,16 +2,19 @@
 
 	var app = angular.module('RushApp', ['RushFactory', 'ngTable']);
 	
+	
+
 	app.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 	}]);
 
-	app.controller('RushTableController', ['Rush', 'NgTableParams' function(Rush, NgTableParams){
+	app.controller('RushTableController', ['Rush', 'NgTableParams', function(Rush, NgTableParams){
 		var vm = this;
-		var self = this;
 		this.rushes = Rush.query();
-		var data = this.rushes;
-		self.tableParams = new NgTableParams({}, {data: data});
+		this.tableParams = new NgTableParams({page: 1, count:10}, {getData: function($defer, params) {
+			this.r
+		}});
 	}]);
+
 })();
