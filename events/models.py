b1a3cd8e975a3d823b48436804d5dc1c357a005c
farid00 <brandman.m@husky.neu.edu>
2015-09-21 23:50:08 -0400

    event rush period

--- events/models.py
+++ events/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from rushtracker.models import Rush
 from organization.models import Organization
 from tenancy.models import TenantAware
+from rushperiod.models import RushPeriod
 class Event(TenantAware):
     title = models.CharField('Event Title', max_length=200)
     description = models.CharField(max_length=2000, blank=True, null=True)
     date = models.DateField('Event Date')
-    attendance = models.ManyToManyField(Rush, null=True, blank=True)
     organization = models.ForeignKey(Organization)
+    rush_period = models.ForeignKey(RushPeriod, null=True, blank=True)
 
     def __str__(self):
         return self.title
