5c8e010f1ec4e5488b6a9a9b3355f6c6a0ec3537
farid00 <brandman.m@husky.neu.edu>
2015-12-08 21:29:49 -0500

    attendance taking commit

--- rushtracker/static/core/mainController.js
+++ rushtracker/static/core/mainController.js
@@ -1,6 +1,6 @@
 (function() {
 
-var rushApp = angular.module('routerApp', ['ui.router', 'RushApp', 'commentApp']);
+var rushApp = angular.module('routerApp', ['ui.router', 'RushApp', 'commentApp', 'eventManagement']);
 
 rushApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
   //
@@ -31,7 +31,31 @@ rushApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     .state('rush.detail', {
       url: "/:id",
       templateUrl: "static/comments/comment_list.html",
-
+    })
+    .state('events', {
+      abstract: true,
+      url: "/events",
+      template: "<div ui-view></div>"
+    })
+    .state('events.list', {
+      url: "/list",
+      templateUrl: "static/events/event_list.html",
+      controller: "EventController as ctrl"
+    })
+    .state('events.detail', {
+      url: "/:id",
+      templateUrl: "static/events/event_detail.html",
+      controller: "EventDetailController as ctrl",
+      resolve: {
+        promiseObj: function($http, $stateParams) {
+          return $http.get('/api/events/' + $stateParams.id);
+        }
+      }
+    })
+    .state('events.attendance' , {
+      url: "/attendance/:id",
+      templateUrl: "static/events/take_attendance.html",
+      controller: "EventAttendanceController as ctrl"
     })
 });
 })();
--- rushtracker/static/events/app.js
+++ rushtracker/static/events/app.js
@@ -5,65 +5,38 @@ var app = angular.module('eventManagement', ['ui.bootstrap', 'ui.select', 'ngSan
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 }]);
 
-	app.controller('EventController', ['$http', '$scope', function($http, $scope) {
+	app.controller('EventController', ['$http', '$state', function($http, $state) {
 		var _this = this;
-		this.testList = [];
 		this.rushList = [];
 		var promise = $http.get('/api/events/');
 
 
 		promise.success(function(data) {
-			_this.rushList = data;
-		});
-
-
-		var promiseRush = $http.get('/api/rush/');
-		promiseRush.success(function(data) {
-			_this.testList = data;
+			_this.eventList = data;
 		});
 	}]);
 
-	//TODO: move all lists into services 
-	app.directive('rushAttendance', function(){
-		return {
-			scope: {
-				event: '=ngModel'
-			},
-			controller: function($http, $scope) {
-				var _this = this;
-				var temp = []
-				var i;
-				for (i = 0; i < this.event.attendance.length; i ++) {
-					temp.push(this.event.attendance[i].id);
-				}
-				//this has to be set first or apply won't be called on
-				//the ui-select because if the first bind is not to a list then 
-				//it will bind to its own internal model, so selected will still be 
-				//available for the model but what previous selects will not be stored
-				//or referenced
-				this.event.attendance = [];
-				console.log(this.event.attendance);
-				this.updateAttendance = function(updateList) {
-					this.response = $http.patch('/api/events/' + this.event.id + '/', {attendance: updateList});
-					this.response.success(function(data) {
-						alert('Update Successfully')
-					});
-				};
+	app.controller('EventDetailController', ['$http', 'promiseObj', function($http, promiseObj) {
+		this.myEvent = promiseObj.data;
+	}]);
 
-				var promiseRush = $http.get('/api/rush/');
-				promiseRush.success(function(data) {
-					_this.testList = data;
-					_this.event.attendance = temp;
-				});
-				console.log('me');
-				console.log(this.event.attendance);
-			},
-			controllerAs: 'ctrl',
-			bindToController: true,
-			require: 'ngModel',
-			restrict: 'E',
-			templateUrl: '/static/directives/attendanceDropDown.html',
-		};
-	});
+	app.controller('EventAttendanceController', ['$http', '$stateParams', function($http, $stateParams) {
+		var _this = this;
+		this.attendance = [];
+		$http.get('/api/rush/').then(function(data) {
+			_this.rushes = data.data.results;
+		});
+		$http.get('/api/events/' + $stateParams.id + '/').then(function(data) {
+			_this.myEvent = data.data;
+			_this.attendance = _this.myEvent.attendance.map(function(obj) {
+				return obj.id;
+			});
+		});
+		this.addAttendance = function(rushId) {
+			console.log(_this.attendance);
+			_this.attendance.push(rushId);
+			$http.patch('/api/events/' + $stateParams.id + '/', {'attendance': _this.attendance});
+		}
+	}]);
 
 })();
